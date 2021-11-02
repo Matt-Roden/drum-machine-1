beaecbd2de7fe497a468d156a50b3c3f4ab40004
Matt Roden <mattroden92@gmail.com>
2021-11-02 17:22:09 -0600

    Add volume fader

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PadsContainer from './PadsContainer';
 import '../styles/App.scss';
 import audioBank from '../audioData'
 
 const App = () => {
+  const [volume, setVolume] = useState(1)
+
   return (
     <div className="App">
       <PadsContainer audioBank={audioBank} key={Date.now()}/>
+      <div className='controls'> 
+        <h4>Volume</h4>
+        <input 
+          className='volume-fader' 
+          type='range' 
+          step='0.01' 
+          value={volume} 
+          max='1' 
+          min='0'
+          onChange={(e) => setVolume(e.target.value)}  
+        />
+      </div>
     </div>
   );
 }
